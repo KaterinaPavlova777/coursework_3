6ef6b32886c437b32a13164938fef70430d7b7b5
Ekaterina Pavlova <miss.kat.katerina2007@gmail.com>
2024-10-21 17:08:48 +0300

    added class JSONSaver

---
+++ src/JSONSaver.py
@@ -0,0 +1,46 @@
+import json
+from typing import Any
+
+from src.vacancy import Vacancy
+
+
+class JSONSaver:
+
+    def __init__(self, filename: str) -> None:
+        self.__filename = filename
+        with open(f"{filename}.json", "w", encoding="utf8") as file:
+            json.dump([], file)
+
+    @property
+    def filename(self) -> Any:
+        return self.__filename
+
+    def add_vacancy(self, vacancy: Vacancy) -> None:
+        data = {
+            "name": vacancy.name,
+            "link": vacancy.link,
+            "salary": vacancy.salary,
+            "description": vacancy.description,
+        }
+
+        with open(f"{self.__filename}.json", "r", encoding="utf8") as file:
+            info = json.load(file)
+        info.append(data)
+
+        with open(f"{self.__filename}.json", "w", encoding="utf8") as file:
+            json.dump(info, file)
+
+    def remove_vacancy(self, vacancy: Vacancy) -> None:
+        data = {
+            "name": vacancy.name,
+            "link": vacancy.link,
+            "salary": vacancy.salary,
+            "description": vacancy.description,
+        }
+
+        with open(f"{self.__filename}.json", "r", encoding="utf8") as file:
+            info = json.load(file)
+        info.remove(data)
+
+        with open(f"{self.__filename}.json", "w", encoding="utf8") as file:
+            json.dump(info, file)
